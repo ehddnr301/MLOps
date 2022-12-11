67fd545c4bf635348a98af8c95cac567cd1d296c
ehddnr301 <dy950328@gmail.com>
2022-12-11 12:46:58 +0000

    Add mlflow log_artifact & Update Preprocess #8

--- pipeline_with_model_registry/model_training.py
+++ pipeline_with_model_registry/model_training.py
@@ -1,10 +1,13 @@
+import os
 import sys
 from datetime import datetime, timedelta
 
 import mlflow
+import pickle
 
 import pandas as pd
 from prefect import flow, task
+from prefect.task_runners import SequentialTaskRunner
 from sqlalchemy import create_engine
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LinearRegression
@@ -29,15 +32,26 @@ def read_data_from_database(query, engine):
 
 
 @task
-def preprocessing(train_df, valid_df, country):
+def get_data_list():
+    return None
+
+
+@task
+def preprocessing(train_df, valid_df, country: str, date: str):
     features = ["Open", "High", "Low", "Close", "Volume"]
     scaler = MinMaxScaler()
 
     train_df = train_df[train_df["company_name"] == country].copy().reset_index()
     valid_df = valid_df[valid_df["company_name"] == country].copy().reset_index()
 
-    train_df["TomorrowClosePrice"] = train_df["Close"].shift(-1)
-    train_df.dropna(axis=0, inplace=True)
+    df = pd.concat([train_df, valid_df], axis=0, ignore_index=True)
+    df["TomorrowClosePrice"] = df["Close"].shift(-1)
+
+    idx = int(df[df["Date"] == date].index[0])
+
+    train_df = df.iloc[: idx + 1].copy().reset_index()
+    valid_df = df.iloc[idx + 1 :].copy().reset_index()
+
     train_feature_df = train_df[features]
 
     train_X = scaler.fit_transform(train_feature_df)
@@ -48,7 +62,7 @@ def preprocessing(train_df, valid_df, country):
     valid_feature_df = valid_df[features]
     valid_X = scaler.transform(valid_feature_df)
 
-    return train_X, train_Y, valid_X, valid_Y
+    return train_X, train_Y, valid_X, valid_Y, scaler
 
 
 @task
@@ -60,22 +74,21 @@ def train_model(train_X, train_Y):
 
 
 @task
-def create_prediction_info(model, valid_X, valid_Y):
+def create_metric(model, valid_X, valid_Y):
     pred = pd.Series(model.predict(valid_X))
     df = pd.concat([valid_Y, pred], axis=1, ignore_index=True)
     df.columns = ["TomorrowClosePrice", "PredictionResult"]
     df.dropna(inplace=True)
-
     msle = mean_squared_log_error(df["TomorrowClosePrice"], df["PredictionResult"])
     mae = mean_absolute_error(df["TomorrowClosePrice"], df["PredictionResult"])
     mse = mean_squared_error(df["TomorrowClosePrice"], df["PredictionResult"])
 
-    info_dict = {
+    metric_dict = {
         "msle": msle,
         "mae": mae,
         "mse": mse,
     }
-    return info_dict
+    return metric_dict
 
 
 @task
@@ -88,24 +101,36 @@ def log_info(logging_info):
                 mlflow.log_metrics(value)
             if key == "model":
                 mlflow.sklearn.log_model(value, artifact_path="stock")
+            if key == "artifacts":
+                ran = int(datetime.now().timestamp())
+                with open(f"preprocessor{ran}.b", "wb") as f_out:
+                    pickle.dump(value, f_out)
+                mlflow.log_artifact(f"preprocessor{ran}.b", artifact_path="stock")
+                os.remove(f"preprocessor{ran}.b")
 
 
-@flow
-def train_model_flow(train_df, valid_df, country_list, date):
+@flow(task_runner=SequentialTaskRunner())
+def train_model_flow(train_df, valid_df, country_list: list, date: str):
     for country in country_list:
-        train_X, train_Y, valid_X, valid_Y = preprocessing(train_df, valid_df, country)
+        train_X, train_Y, valid_X, valid_Y, scaler = preprocessing(
+            train_df, valid_df, country, date
+        )
         model = train_model(train_X, train_Y)
-        info_dict = create_prediction_info(model, valid_X, valid_Y)
+        metric_dict = create_metric(model, valid_X, valid_Y)
 
         params = {"country": country, "date": date}
-
-        logging_info = {"params": params, "metrics": info_dict, "model": model}
+        logging_info = {
+            "params": params,
+            "metrics": metric_dict,
+            "model": model,
+            "artifacts": scaler,
+        }
 
         log_info(logging_info)
 
 
-@flow
-def training_flow(date):
+@flow(task_runner=SequentialTaskRunner())
+def training_flow(date: str):
     if date == "daily":
         date = str((datetime.today() + timedelta(hours=9) - timedelta(days=30)).date())
 
@@ -117,7 +142,8 @@ def training_flow(date):
         SELECT * FROM stocktable WHERE "Date" >= '{date}' 
     """
 
-    COUNTRY_LIST = ["APPLE", "GOOGLE", "MICROSOFT", "AMAZON"]
+    COUNTRY_LIST = get_data_list() or ["APPLE", "GOOGLE", "MICROSOFT", "AMAZON"]
+
     engine = create_db_engine(HOST_URL)
     train_df = read_data_from_database(TRAIN_QUERY, engine)
     valid_df = read_data_from_database(VALID_QUERY, engine)
